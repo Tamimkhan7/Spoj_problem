e00f0e017e7b5abe9e89f4779faf492d38cbc6b8
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-02-18 20:05:51 +0600

    first commit

---
+++ TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    for(int i=1; i<=1; i--)
+    {
+        cin>>n;
+        if(n == 42)
+        {
+            break;
+        }
+        cout<<n<<endl;
+    }
+}
